47dc381c07649f2a3dca2cbaada985d68a2948c3
nickmancari <nickjmancari@gmail.com>
2021-02-15 19:59:01 +0000

    constructing Stdin use

--- mem.go
+++ mem.go
@@ -3,13 +3,21 @@ package main
 import (
 
 	"fmt"
-
+	"bufio"
+	"log"
+	"os"
 )
-
 
 func main() {
 
-	fmt.Println("\u2610 Not Done")
-	fmt.Println("\u2611 Done")
-
+	fmt.Println("What Do You Need To Do? ")
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		fmt.Println("[ ] ", scanner.Text())
+	}
+	
+	if err := scanner.Err(); err != nil {
+		log.Println(err)
+	}
 }
+
