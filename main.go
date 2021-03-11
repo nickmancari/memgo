a800f8d6b6d64509c60024fd2b66b577773eb1ad
nickmancari <nickjmancari@gmail.com>
2021-03-11 14:09:25 +0000

    changed notes

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	fname := "todo.md"
 
-	//take user input and print it to "todo.md" file
+	//open "todo.md" file where the list will be saved & show the content
 	textin, err := ioutil.ReadFile(fname)
 	if err == nil {
 		cyan := color.New(color.FgCyan).PrintFunc()
@@ -23,7 +23,7 @@ func main() {
 	
 	scanner := bufio.NewScanner(os.Stdin)
 	
-	//stop the program and write the input to file
+	//take in user input and and stop taking input when "." is entered on a new line
 	for scanner.Scan() {
 		line := scanner.Text()
 		if line == "." {
@@ -32,7 +32,7 @@ func main() {
 		userInput = append(userInput, line+"\n")
 	}
 	
-	//append text to file
+	//write the scanned text to the file and add empty check box to the item
 	f, err := os.OpenFile(fname, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
 	defer f.Close()
 	
@@ -45,7 +45,7 @@ func main() {
 		}
 	}
 	
-	//print text 
+	//open "todo.md" again, showing the newly added content
 	textin, err = ioutil.ReadFile(fname)
 	if err != nil {
 		panic(err)
